7daac5a0b1f04649ee2a3df8751ecd3c592f0a73
Max Chadwick <mpchadwick@gmail.com>
2017-04-06 19:21:20 -0700

    Add ability to hide count

--- Model/DataProvider.php
+++ Model/DataProvider.php
@@ -9,8 +9,10 @@ use Magento\Store\Model\ScopeInterface;
 class DataProvider
 {
     const CONFIG_AUTOCOMPLETE_LIMIT = 'catalog/search/autocomplete_limit';
+    const CONFIG_HIDE_RESULT_COUNT = 'catalog/search/hide_result_count';
 
     protected $limit;
+    protected $hideResultCount;
 
     public function __construct(ScopeConfig $scopeConfig)
     {
@@ -18,19 +20,30 @@ class DataProvider
             self::CONFIG_AUTOCOMPLETE_LIMIT,
             ScopeInterface::SCOPE_STORE
         );
+
+        $this->hideResultCount = (bool) $scopeConfig->getValue(
+            self::CONFIG_HIDE_RESULT_COUNT,
+            ScopeInterface::SCOPE_STORE
+        );
     }
 
     public function afterGetItems(Subject $subject, array $items)
     {
-        // Note: We intercept here (rather than intercepting the db query
-        // and setting a limit) because there is logic in getItems
-        // which bumps current query to front, if found
+        if ($this->hideResultCount) {
+            foreach ($items as $k => $v) {
+                unset($v['num_results']);
+            }
+        }
 
         // todo: Magento\Search\Model\Autocomplete::getItems will merge
         // the results of each provider. So it would be more if additional
         // providers were added. OOB Magento only uses
         // Magento\CatalogSearch\Model\Autocomplete\DataProvider, but need
         // to determine if this is the best place
+
+        // Note: We intercept here (rather than intercepting the db query
+        // and setting a limit) because there is logic in getItems
+        // which bumps current query to front, if found
         return ($this->limit) ? array_splice($items, 0, $this->limit) : $items;
     }
 }
